67aac343fed989c99e28cd2023ea0c59efb0b253
Sandeshkamble07 <kambale.sandesh777@gmail.com>
2025-01-01 21:25:20 +0530

    Add files via upload

---
+++ jenkinsexercise/src/main/java/com/exercise/jenkinserercise/JenkinsererciseApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.jenkinserercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsererciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsererciseApplication.class, args);
+	}
+
+}
