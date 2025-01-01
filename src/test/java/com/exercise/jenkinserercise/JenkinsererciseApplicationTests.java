be40e64b042bd506ff81cf0b7916259e884834fb
Sandeshkamble07 <kambale.sandesh777@gmail.com>
2025-01-01 21:21:50 +0530

    Add files via upload
    initial commit

---
+++ src/test/java/com/exercise/jenkinserercise/JenkinsererciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.jenkinserercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsererciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
